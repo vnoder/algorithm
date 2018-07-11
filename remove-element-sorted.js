8f6005cffee72b86aa24d4dc94e2f1d4b2945595
vardking <vardking@gmail.com>
2018-07-11 21:03:52 +0800

    remove duplicates value from array,duplicates are allowed at most twice

--- remove-element-sorted.js
+++ remove-element-sorted.js
@@ -5,7 +5,9 @@
 // Your function should return length = 2, and A is now [1,2].
 
 'use strict';
-module.exports = removeValue;
+module.exports = {
+    removeValue, removeValueAllowTwice
+};
 
 /**
  * remove specified value from array
@@ -25,6 +27,40 @@ function removeValue(arr) {
 
         if (arr[j] != arr[i] && i - j > 1) {
             j++;
+            arr[j] = arr[i];
+            continue;
+        }
+
+        j++;
+    }
+    return j + 1;
+}
+
+// Follow up for "Remove Duplicates": What if duplicates are allowed at most twice?
+// For example, Given sorted array A = [1,1,1,2,2,3],
+// Your function should return length = 5, and A is now [1,1,2,2,3].
+/**
+ * remove specified value from array,allow same value at most twice
+ * @param {*} arr 
+ */
+function removeValueAllowTwice(arr) {
+    if (arr.length <= 1) {
+        return arr.length;
+    }
+
+    let j = 0;
+    let tmp = 0;
+    for (let i = 1, len = arr.length; i < len; i++) {
+        if (arr[j] == arr[i]) {
+            tmp++;
+            if(tmp <= 2){
+                j++;
+            }
+            continue;
+        }
+
+        if (arr[j] != arr[i] && i - j > 1) {
+            tmp = 0;
             arr[j] = arr[i];
             continue;
         }
